87445575c026caff9900d185c4ba1221b6e7c736
Rohan <99284516+Shetty852@users.noreply.github.com>
2025-07-09 21:56:39 +0530

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,7 @@ const taskSchema = new mongoose.Schema({
   task: { type: String, required: true },
   assignedTo: { type: String, required: true },
   date: { type: Date, required: true },
-  status: { type: String, enum: ['Not Done', 'Done'], default: 'Not Done' } // ✅ Add this
+  status: { type: String, enum: ['Not Done', 'Done'], default: 'Not Done' } 
 });
 
 module.exports = mongoose.model('Task', taskSchema);
